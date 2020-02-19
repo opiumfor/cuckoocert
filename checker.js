da79c3ada92172f8359f25e4f471e222ad0d22d9
Ruslan Zanoch <ruslan@ucoz-team.net>
2020-02-19 08:30:39 +0300

    checker.js первый коммит

---
+++ checker.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const addHttps = url => {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = 'https://' + url;
+  }
+  return url;
+};
+
+const request = axios.create({
+  timeout: 10000,
+  method: 'HEAD',
+  headers: { 'User-Agent': 'cuckoocert' },
+  maxRedirects: 0,
+  validateStatus: status => {
+    return status >= 200 && status < 499;
+  },
+});
+
+const getCertificate = async uri => {
+  return await request(addHttps(uri)).then(response => {
+    return response.request;
+  });
+};
+
+const getCertificateExpiryDate = async uri => {
+  return await request(addHttps(uri)).then(response => {
+    return response.request.socket.getPeerCertificate().valid_to;
+  });
+};
+
+module.exports = { getCertificateExpiryDate, getCertificate };
--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { ENDPOINTS_LIST_URI: endpointsListURI } = process.env;
 
 const cuckoocert = require('./cuckoocert');
+const checker = require('./checker');
 const argv = require('yargs')
   // show help if no command given
   .demandCommand(1, '')
@@ -14,6 +15,24 @@ const argv = require('yargs')
     async argv => {
       const endpointsList = await cuckoocert.getEndpointsListFromURI(endpointsListURI);
       console.log(endpointsList);
+    },
+  )
+  .command(
+    'when <uri>',
+    'Show expiry date for single endpoint',
+    () => {},
+    async argv => {
+      const expiryDate = await checker.getCertificateExpiryDate(argv.uri);
+      console.log(expiryDate);
+    },
+  )
+  .command(
+    'cert <uri>',
+    'Show certificate info for single endpoint',
+    () => {},
+    async argv => {
+      const certificate = await checker.getCertificate(argv.uri);
+      console.log(certificate);
     },
   )
   .command(
